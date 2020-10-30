909de6b0d34eff21a55ddf9f195165edd0ace8a9
Chendra Sena Oemaryoga <chendrasena.oe@gmail.com>
2020-10-30 13:33:30 +0700

    adjust navbar

--- assets/custom/js/custom-js.js
+++ assets/custom/js/custom-js.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
             scrollTop: $(".gallery").offset().top},
             'medium');
     });
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar-new").style.top = "0";
+    } else {
+        document.getElementById("navbar-new").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+    }
   });
